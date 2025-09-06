6623217e55321880c1e3f2b3173f690d4777cb02
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-09-06 20:51:54 +0300

    Updated relationshiip_app/

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 from django.http import HttpResponse
-from django.views.generic import DetailView
 from .models import Library, Book
 
 
