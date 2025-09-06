7c7202122625d54c64c512729e41dfd27ba7627b
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-09-06 20:58:14 +0300

    Updated relationshiip_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views
+from .views import list_books
 
 
 urlpatterns = [
     # Function-based view URL
-    path('books/', views.book_list, name='book-list'),
+    path('books/', views.list_books, name='list-books'),
     
     # Class-based view URLs
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
