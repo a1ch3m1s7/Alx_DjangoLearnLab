493c768c0d15da1fed2c6c23303f38cc8997e4a8
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-09-06 21:48:15 +0300

    Added authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 
@@ -10,9 +11,11 @@ urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
     # path('libraries/', views.LibraryListView.as_view(), name='library-list'),
 
-     # Authentication
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
+    # Authentication using Django's built-in views
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+
+    # Registration (still custom)
+    path("register/", views.register_view, name="register")
 ]
 
