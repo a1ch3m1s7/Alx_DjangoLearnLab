e6bf640e6fe1e6679a96bab23685700dabe61ee7
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-09-21 23:32:07 +0300

    Updated Token Authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics, viewsets
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
+	permission_classes = [AllowAny]
 
 class BookViewSet(viewsets.ModelViewSet):
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
+	permission_classes = [IsAuthenticated]
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),  # Token endpoint
 ]
