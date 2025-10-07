20053fbd88317123d5724a5447f6606fe1b0634e
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-10-07 11:26:07 +0300

    Updated the tagging using django-taggit

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 from .models import Profile, Post, Comment, Tag
 
 class UserRegistrationForm(UserCreationForm):
@@ -29,6 +30,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
 
     def save(self, commit=True, *args, **kwargs):
         instance = super().save(commit=False)
@@ -47,13 +51,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
-        widgets = {
-            'content': forms.Textarea(attrs={
-                'placeholder': 'Write your comment...',
-                'rows': 4,
-                'class': 'form-control'
-            }),
-        }
+        # widgets = {
+        #     'content': forms.Textarea(attrs={
+        #         'placeholder': 'Write your comment...',
+        #         'rows': 4,
+        #         'class': 'form-control'
+        #     }),
+        # }
         labels = {
             'content': ''
         }
