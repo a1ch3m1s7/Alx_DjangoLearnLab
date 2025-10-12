ade669e9630506a62eaafad8289ddc46f4cc9938
a1ch3m1s7 <a1ch3m1s7@outlook.com>
2025-10-12 23:17:24 +0300

    Edited Serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,13 @@
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
-from django.contrib.auth import authenticate
 from rest_framework.authtoken.models import Token
-from .models import CustomUser
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 
@@ -14,11 +15,11 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password']
